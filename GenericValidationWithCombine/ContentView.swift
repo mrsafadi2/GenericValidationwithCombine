f846939caafb03b7dfcd7f5eb789065895990f51
mrsafadi2 <mrsafadi2@gmail.com>
2022-12-24 16:50:51 +0200

    Initial Commit

---
+++ GenericValidationWithCombine/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GenericValidationWithCombine
+//
+//  Created by Mohammed Safadi Macbook Pro on 24/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GenericValidationWithCombine/GenericValidationWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericValidationWithCombineApp.swift
+//  GenericValidationWithCombine
+//
+//  Created by Mohammed Safadi Macbook Pro on 24/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GenericValidationWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
